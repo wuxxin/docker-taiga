4ef9559205f0286753d1f3aa8c76ccf8514d1a55
Salt Master <saltmaster@localhost>
2016-04-22 02:28:29 +0200

    update latest

--- conf/taiga/frontend-extra-download.sh
+++ conf/taiga/frontend-extra-download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p /app/taiga-front-dist/dist/plugins
 cd /app/taiga-front-dist/dist/plugins
+svn export "https://github.com/taigaio/taiga-contrib-gogs/tags/$(pip show taiga-contrib-gogs | awk '/^Version: /{print $2}')/front/dist" "gogs"
+# wget "https://raw.githubusercontent.com/taigaio/taiga-contrib-gogs/$(pip show taiga-contrib-gogs | awk '/^Version: /{print $2}')/front/dist/gogs.js"
--- conf/taiga/local.py
+++ conf/taiga/local.py
@@ -1,11 +1,4 @@
-# If you want to modify this file, I recommend check out docker-taiga-example
-# https://github.com/benhutchins/docker-taiga-example
-#
-# Please modify this file as needed, see the local.py.example for details:
-# https://github.com/taigaio/taiga-back/blob/master/settings/local.py.example
-#
-# Importing docker provides common settings, see:
-# https://github.com/benhutchins/docker-taiga/blob/master/docker-settings.py
-# https://github.com/taigaio/taiga-back/blob/master/settings/common.py
-
 from .docker import *
+#
+INSTALLED_APPS += ["taiga_contrib_gogs"]
+PROJECT_MODULES_CONFIGURATORS["gogs"] = "taiga_contrib_gogs.services.get_or_generate_config"
